aa1d05f73baee2469d7a29b93e04f89bc6dc651d
Tony133 <a.tripodi133@gmail.com>
2020-01-25 23:40:09 +0100

    Added name on api routes

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,11 @@ $api = app(Router::class);
 
 $api->version('v1', function (Router $api) {
 	$api->group(['prefix' => 'auth'], function(Router $api) {
-		$api->post('signup', 'App\\Api\\V1\\Controllers\\SignUpController@signUp');
-		$api->post('login', 'App\\Api\\V1\\Controllers\\LoginController@login');
+		$api->post('signup', 'App\\Api\\V1\\Controllers\\SignUpController@signUp')->name('signup');
+		$api->post('login', 'App\\Api\\V1\\Controllers\\LoginController@login')->name('login');
 
-		$api->post('recovery', 'App\\Api\\V1\\Controllers\\ForgotPasswordController@sendResetEmail');
-		$api->post('reset', 'App\\Api\\V1\\Controllers\\ResetPasswordController@resetPassword');
+		$api->post('recovery', 'App\\Api\\V1\\Controllers\\ForgotPasswordController@sendResetEmail')->name('password.forgot');
+		$api->post('reset', 'App\\Api\\V1\\Controllers\\ResetPasswordController@resetPassword')->name('password.reset');
 	});
 
 	$api->group(['middleware' => 'jwt.auth'], function(Router $api) {
